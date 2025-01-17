eb144628e3635df0fb939cebec9f3d56ccad0c9c
karoljanowski <karol.jj@icloud.com>
2025-01-17 18:24:26 +0100

    update scraper

--- src/lib/actions/scraper.ts
+++ src/lib/actions/scraper.ts
@@ -37,6 +37,8 @@ export const getHTMLFromLink = async (link: string) => {
 }
 
 export const scrapOfferData = async (html: string): Promise<ScraperResponse> => {
+    console.log("HTML:", html)
+
     if (!html) {
         return { success: false, error: "Failed to get content from link" }
     }
@@ -50,7 +52,7 @@ export const scrapOfferData = async (html: string): Promise<ScraperResponse> => 
                     "content": [
                         {
                             "type": "text",
-                            "text": "Extract relevant data from provided HTML content. Structure your response in JSON format."
+                            "text": "Extract relevant data from provided HTML content. Structure your response in JSON format. If HTML is not an offer, return an empty object."
                         }
                     ]
                 },
@@ -60,15 +62,6 @@ export const scrapOfferData = async (html: string): Promise<ScraperResponse> => 
                         {
                             "type": "text",
                             "text": html
-                        }
-                    ]
-                },
-                {
-                    "role": "assistant",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": "{\n  \"company\": \"Sii Polska\",\n  \"position\": \"Bioinformatics Engineer - pharmaceutical industry\",\n  \"location\": \"Warsaw, Gdansk, Wroclaw, Poznan, Krakow, Lodz, Lublin, Katowice, Rzeszów, Częstochowa, Piła, Bydgoszcz, Białystok, Gliwice, Szczecin, Torun\",\n  \"expiresAt\": \"2025-02-15T00:00:00Z\",\n  \"requirements\": \"<ul>\\n<li>Over 4 years of professional experience in R and Python programming, preferably in bioinformatics projects</li>\\n<li>Knowledge of Bash and Linux, as well as relational databases and containerization</li>\\n<li>Good coding practices of Test Driven Development (simultaneous work with other team members on the same code base, code review, and feedback system)</li>\\n<li>Cooperative skills with teams from different backgrounds, e.g. tech - non-tech, backend - frontend</li>\\n<li>Communicate freely in Polish and English</li>\\n<li>Residing in Poland required</li>\\n</ul>\\n<p><strong>Nice-to-have requirements</strong></p>\\n<ul><li>Biological (or related) educational background</li></ul>\",\n  \"description\": \"<p>Join our team working on projects of one of the largest pharmaceutical companies in the world and have a real impact on the lives and health of more than 15 million patients!</p>\\n<p>Take part in building a custom portal that will enable scientists to integrate the next generation of scientific insights.</p>\\n<p><strong>Your tasks</strong></p>\\n<ul>\\n<li>Developing science pipelines using R and Python technologies</li>\\n<li>Designing algorithms in close collaboration with stakeholders or adopting and creating stakeholder algorithms</li>\\n<li>Providing code reviews for other team members</li>\\n<li>Participating in cross-team, internal, or business meetings, gathering requirements from stakeholders and other teams</li>\\n<li>Performing analysis related to genomics or other biological data</li>\\n</ul>\"\n}"
                         }
                     ]
                 }
