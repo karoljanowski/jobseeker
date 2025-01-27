5fa68d6c94cc0134fb2c7e733ba320cd8ea83c21
karoljanowski <karol.jj@icloud.com>
2025-01-27 23:48:27 +0100

    debug

--- src/lib/actions/scraper.ts
+++ src/lib/actions/scraper.ts
@@ -37,10 +37,13 @@ export const getCleanHTMLFromLink = async (link: string) => {
     try {
         const response = await axios.get(link, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Referer': 'https://www.google.com/',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
             }
         });
-        console.error(response)
+
         if (response.status !== 200) {
             console.error("Failed to fetch content from link:", response.status, response.statusText);
             return { success: false, error: "Failed to get content from link" };
