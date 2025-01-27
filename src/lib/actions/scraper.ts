7a7737a4d582d9246dcc3216953616cb4521ad03
karoljanowski <karol.jj@icloud.com>
2025-01-27 23:18:52 +0100

    fix scraper

--- src/lib/actions/scraper.ts
+++ src/lib/actions/scraper.ts
@@ -34,7 +34,12 @@ export const getLastGptUsage = async (userId: number) => {
 
 export const getHTMLFromLink = async (link: string) => {
     try {
-        const response = await fetch(link)
+        const response = await fetch(link, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+        })
+
         if (!response.ok) {
             return { success: false, error: "Failed to get content from link" }
         }
