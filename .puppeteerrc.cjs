6b749bf5f7a81441eff4044780998f99e4e2566d
karoljanowski <karol.jj@icloud.com>
2025-03-05 22:34:16 +0100

    test

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,10 @@
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+    // Download Chrome (default `skipDownload: false`).
+    chrome: {
+      skipDownload: false,
+    }
+  };
